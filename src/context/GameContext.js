10d1559405baf3e7e726665e1cc7ea69b3a61ea1
spiderforrest <spider@spood.org>
2023-01-25 14:57:45 -0800

    i grabbed out that big function? i have no idea how much of this i should be refactoring so i'm going ham

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -11,6 +11,32 @@ export const GameProvider = ({ children }) => {
   const [playerThreeHand, setPlayerThreeHand] = useState([]);
   const [from, setFrom] = useState('deck');
   const [to, setTo] = useState(1);
+
+  function findCardIndex(value, suit, cards) {
+    return cards.findIndex((card) => card.value === value && card.suit === suit);
+  }
+
+  function passCard(card) {
+    const playerHands = [playerOneHand, playerTwoHand, playerThreeHand];
+    const playerHandSetFunctions = [setPlayerOneHand, setPlayerTwoHand, setPlayerThreeHand];
+
+    // arrays start at zero, but our players start at 1 :shrug:
+    const toHand = playerHands[to - 1] || deck;
+    const fromHand = playerHands[from - 1] || deck;
+
+    const toSetFunction = playerHandSetFunctions[to - 1] || setDeck;
+    const fromSetFunction = playerHandSetFunctions[from - 1] || setDeck;
+
+    const cardToMoveIndex = findCardIndex(card.value, card.suit, fromHand);
+    const [cardToMove] = fromHand.splice(cardToMoveIndex, 1);
+
+    toHand.push(cardToMove);
+
+    toSetFunction([...toHand]);
+    fromSetFunction([...fromHand]);
+
+    setSelectedCard(null);
+  }
   return (
     <GameContext.Provider
       value={{
@@ -28,6 +54,7 @@ export const GameProvider = ({ children }) => {
         setFrom,
         to,
         setTo,
+        passCard,
       }}
     >
       {children}
