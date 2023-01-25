0eddcbd6b59fb113ef1d2215a7e96b4a65b29bc6
spiderforrest <spider@spood.org>
2023-01-25 15:13:44 -0800

    ahh now i get it, fixed

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,8 +2,7 @@ import React, { useContext } from 'react';
 import Card from './Card';
 import { GameContext } from '../context/GameContext.js';
 
-export default function CardList({ player }) {
-  const { deck: cards } = useContext(GameContext);
+export default function CardList({ player, cards }) {
   return (
     <div className="card-list">
       {cards.map((card) => (
